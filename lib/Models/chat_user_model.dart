9048d9a69ed2a09729e20e03f603c5e2c8183027
sushil <baymax732001@gmail.com>
2023-08-25 07:52:34 +0530

    firebase

---
+++ lib/Models/chat_user_model.dart
@@ -0,0 +1,18 @@
+// import 'package:equatable/equatable.dart';
+
+// class ChatUser extends Equatable {
+//   final String id;
+//   final String phototUrl;
+//   final String displayName;
+//   final String phoneNumber;
+//   final String aboutMe;
+//   const ChatUser({
+//     required this.id,
+//     required this.phototUrl,
+//     required this.displayName,
+//     required this.phoneNumber,
+//     required this.aboutMe,
+    
+//   });
+  
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_chat_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
